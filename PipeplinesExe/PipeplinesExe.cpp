eceb6300b6ea14ba170d032459e346184ff54b45
kpraneethkumar <50856129+kpraneethkumar@users.noreply.github.com>
2023-04-14 20:06:15 -0400

    Update PipelinesExe.cpp for cout changes

--- PipeplinesExe/PipeplinesExe.cpp
+++ PipeplinesExe/PipeplinesExe.cpp
@@ -6,7 +6,9 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World! This is Group 27!!!!!!!\n";
+    std::cout << "Praneeth Kumar Karlapati - M15194776\n";
+    std::cout << "Himakar Ananthasetty - M15171777\n";
     CoolClass coolClass;
     std::cout << "Calc " << coolClass.Calc(1, 2);
 }
